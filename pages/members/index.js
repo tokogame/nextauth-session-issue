42f0446af1c9167887422d5e66f19f73dfbdf354
mtando <michaeltando@gmail.com>
2023-11-08 00:44:09 +0700

    Removed next-seo

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { NextSeo } from "next-seo";
 
 function index() {
-  return (
-    <div>
-      <NextSeo noindex={true} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default index;
